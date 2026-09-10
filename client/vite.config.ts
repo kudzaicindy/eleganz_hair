99cdc7d163e39ea1ce3158e927d2cd0649bb21a3
kudzaicindy <165305935+kudzaicindy@users.noreply.github.com>
2026-09-10 16:54:10 +0200

    fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
   server: {
     host: true,
     port: 5173,
